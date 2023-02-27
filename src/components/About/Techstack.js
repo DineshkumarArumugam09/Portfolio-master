6e0e01035d5f61d209dc6b85ece6da82ced928b6
Dineshkumar A <dinesh@constient.com>
2023-02-27 22:49:09 +0530

    first commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { AiFillHtml5 } from "react-icons/ai";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  
+  DiGit,
+  DiMsqlServer,
+} from "react-icons/di";
+import {
+  
+  
+  SiCsharp,
+  SiDotnet,
+  SiAdobephotoshop,
+  SiAdobeillustrator,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCsharp />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDotnet />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMsqlServer />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+    
+      <Col xs={4} md={2} className="tech-icons">
+        <AiFillHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobeillustrator />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobephotoshop />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiOutlineTwitter,
+  AiFillInstagram,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  // let date = new Date();
+  // let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        {/* <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by dinesh kumar</h3>
+        </Col> */}
+        {/* <Col md="4" className="footer-copywright">
+          <h3>copyright © {year}cdsgraphics</h3>
+        </Col> */}
+        <Col className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                 href="https://whatsapp.com/dl/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineWhatsApp />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/chezhiyan_adk"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/dinesh-kumar-8a3315233/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                 href="https://www.instagram.com/adk_dinesh"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
